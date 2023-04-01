e29570a9469a5c278f7c14a9795a54bb5c70292b
Patricia <124640646+P-Trish@users.noreply.github.com>
2023-04-01 12:27:29 -0700

    use starter code

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -31,83 +31,82 @@ module.exports = generateMarkdown;
 
 
 
+// const inquirer = require('inquirer');
+// const fs = require('fs');
 
-const inquirer = require('inquirer');
-const fs = require('fs');
-
-inquirer
-    .prompt([
-    {
-    type: 'input',
-    name: 'title',
-    message: 'What is the title of your markdown file?',
-    },
-    {
-    type: 'input',
-    name: 'description',
-      message: 'Provide a short description explaining the what, why, and how of your project.',
-    },
-    {
-    type: 'input',
-    name: 'table of contents',
-    message: 'Enter table of contents to make it easy for users to find what they need. ',
-    },
-    {
-    type: 'input',
-    name: 'installation',
-    message: 'What are the steps required to install your project?',
-    },
-    {
-    type: 'input',
-    name: 'usage',
-    message: 'Provide instructions for usage.',
-    },
-    {
-    type: 'list',
-    name: 'license',
-    message: 'Choose the license used to let other developers know what they can and cannot do with your project.',
-    choices: ['MIT License', 'Apache License 2.0', 'BSD License'],
-    },
-    {
-    type: 'input',
-    name: 'contributing',
-    message: 'Please include guidelines for how other developers may contribute.',
-    },
-    {
-    type: 'input',
-    name: 'tests',
-    message: 'Please provide the tests for your application with examples on how to run them.',
-    },
-    {
-    type: 'input',
-    name: 'questions',
-    message: 'Do you have any questions?',
-    }
-])
+// inquirer
+//     .prompt([
+//     {
+//     type: 'input',
+//     name: 'title',
+//     message: 'What is the title of your markdown file?',
+//     },
+//     {
+//     type: 'input',
+//     name: 'description',
+//       message: 'Provide a short description explaining the what, why, and how of your project.',
+//     },
+//     {
+//     type: 'input',
+//     name: 'table of contents',
+//     message: 'Enter table of contents to make it easy for users to find what they need. ',
+//     },
+//     {
+//     type: 'input',
+//     name: 'installation',
+//     message: 'What are the steps required to install your project?',
+//     },
+//     {
+//     type: 'input',
+//     name: 'usage',
+//     message: 'Provide instructions for usage.',
+//     },
+//     {
+//     type: 'list',
+//     name: 'license',
+//     message: 'Choose the license used to let other developers know what they can and cannot do with your project.',
+//     choices: ['MIT License', 'Apache License 2.0', 'BSD License'],
+//     },
+//     {
+//     type: 'input',
+//     name: 'contributing',
+//     message: 'Please include guidelines for how other developers may contribute.',
+//     },
+//     {
+//     type: 'input',
+//     name: 'tests',
+//     message: 'Please provide the tests for your application with examples on how to run them.',
+//     },
+//     {
+//     type: 'input',
+//     name: 'questions',
+//     message: 'Do you have any questions?',
+//     }
+// ])
       
 
-.then ((response)) => {
-    // consider console logging response to see what the data inside is
-    // build out readme as a string
-    // save to file (`fs`)
-    // I think that's it?
-    let readMe = 
-    // ENTER READ ME TEXT HERE --------------------->
+// .then ((response)) => {
+//     // consider console logging response to see what the data inside is
+//     // build out readme as a string
+//     // save to file (`fs`)
+//     // I think that's it?
+//     let readMe = 
+//     // ENTER READ ME TEXT HERE --------------------->
 
-const filename = `README.md`
-
-
+// const filename = `README.md`
 
 
 
-const filename = `readMe.md`;
 
-fs.writeFile(filename.readMe, (err) =>
-err? console.err (err) : console.log ('README.md Created!')
-)
 
-  });
-}
+// const filename = `readMe.md`;
+
+// fs.writeFile(filename.readMe, (err) =>
+// err? console.err (err) : console.log ('README.md Created!')
+// )
+
+//   });
+// }
 
 
 
