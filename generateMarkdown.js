be620373292b69dd687eb4ba966834543827297b
Patricia <124640646+P-Trish@users.noreply.github.com>
2023-04-03 18:31:21 -0700

    started readme for project

--- generateMarkdown.js
+++ generateMarkdown.js
@@ -38,6 +38,10 @@ function renderLicenseSection(license) {
 }
 
 // TODO: Create a function to generate markdown for README
+
+// WHEN I click on the links in the Table of Contents
+// THEN I am taken to the corresponding section of the README
+
 function generateMarkdown(data) {
 return `# ${data.title} ${renderLicenseBadge(data.license)}
   
@@ -73,12 +77,17 @@ ${data.tests}
 ## Contributing
 ${data.contributing}
 
+
 ## Questions
 
 If you have any questions about the repo, open an issue or contact me directly at ${data.email}. You can find more of my work at [${data.gitHub}](https://github.com/${data.gitHub}/).
   
 `;
 }
+
+
+
+
 
 module.exports = generateMarkdown;
 
@@ -100,19 +109,8 @@ module.exports = generateMarkdown;
 // WHEN I enter a description, installation instructions, usage information, contribution guidelines, and test instructions
 // THEN this information is added to the sections of the README entitled Description, Installation, Usage, Contributing, and Tests
 
-// STILL NEED:
-
-// WHEN I choose a license for my application from a list of options
-// THEN a badge for that license is added near the top of the README and a notice is added to the section of the README entitled License that explains which license the application is covered under
-
-// WHEN I enter my GitHub username
-// THEN this is added to the section of the README entitled Questions, with a link to my GitHub profile
-
-// WHEN I enter my email address
-// THEN this is added to the section of the README entitled Questions, with instructions on how to reach me with additional questions
-
-// WHEN I click on the links in the Table of Contents
-// THEN I am taken to the corresponding section of the README
 
 
-// LAST STEP: markdown file formatting 
+
+
+
--- outputReadme.md
+++ outputReadme.md
@@ -1,9 +1,9 @@
- # TEST [![License](https://img.shields.io/badge/license-MIT-blue.svg)](https://opensource.org/licenses/MIT)
+# TEST [![License](https://img.shields.io/badge/license-MIT-blue.svg)](https://opensource.org/licenses/MIT)
   
-  ## Description
-  TEST
-  
-  ## Table Of Contents
+## Description
+TEST
+
+## Table Of Contents
 - [Installation](#installation)
 - [Usage](#usage)
 - [License](#license)
@@ -11,28 +11,29 @@
 - [Tests](#tests)
 - [Questions](#questions)
 
-  ## Installation
-  TEST
-  
-  ## Usage
-  TEST
-  
-  ## Credits
-  undefined
-  
-  ## License
-  https://opensource.org/licenses/MIT
-  
-  ## Contributing
-  TEST
-  
-  ## Tests
-  TEST
-  
-  ## Contributing
-  TEST
-  
-  ## Questions
+## Installation
+TEsT
 
-  If you have any questions about the repo, open an issue or contact me directly at TEST. You can find more of my work at [TEST](https://github.com/TEST/).
+## Usage
+TEST
+
+## Credits
+undefined
+
+## License
+https://opensource.org/licenses/MIT
+
+## Contributing
+TEST
+
+## Tests
+TEST
+
+## Contributing
+TEST
+
+
+## Questions
+
+If you have any questions about the repo, open an issue or contact me directly at pkfalberto@gmail.com. You can find more of my work at [P-Trish](https://github.com/P-Trish/).
   
